fe96b8488f273e10681f85a3e9ddecb78eded9f8
haseeb2222 <haseebabdulla@gmail.com>
2022-02-02 14:28:59 +0530

    version1

---
+++ myphone/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+
+# Create your views here.
+def home(request):
+    return render(request,"homepage.html")
+
+def login(request):
+    return render(request,"loginpage.html")
+
+
+
+
+
+
